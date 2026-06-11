d874b517980987fb9b5be5e2c49c45004ee92451
Robert Nazaryan <robertnazaryan0@gmail.com>
2026-06-11 20:05:53 +0400

    tmp

--- src/services/api.ts
+++ src/services/api.ts
@@ -15,7 +15,7 @@ import type {
   CardAddRequest
 } from '../types';
 
-export const API_URL = 'http://localhost:8080/api';
+export const API_URL = 'https://gofundmebe-production.up.railway.app/api';
 
 const api: AxiosInstance = axios.create({
   baseURL: API_URL,
